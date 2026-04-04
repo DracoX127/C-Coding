aed4b627bb0f3e8f026d30d8015def18ce6203d4
DracoX127 <terry.tw.chang@gmail.com>
2026-04-04 11:25:02 -0700

    A457 - Zerojudge

--- Zerojudge/A457.cpp
+++ Zerojudge/A457.cpp
@@ -7,26 +7,50 @@ const int MaxK = 1e5;
 const long long MaxM = 2e9;
 long long K, M;
 
+int RangeQuery(vector<int>& BIT, int p){
+    int ret = 0;
+    while(0 < p){
+        ret += BIT[p];
+        p -= p & -p;
+    }
+    return ret;
+}
+
+void PointUpdate(vector<int>& BIT, int p, int x){
+    while(p <= K){
+        BIT[p] += x;
+        p += p & -p;
+    }
+}
+
 struct REST{
-    int first, second;
+    int first, second; // first score for restaurant[a], second score for restaurant[b]
 };
 REST scores[MaxK];
 
 int main(){
     cin >> K >> M;
     for(int i = 0; i < K; i++) cin >> scores[i].first;
-    for(int i = 0; i < M; i++) cin >> scores[i].second;
+    for(int i = 0; i < K; i++) cin >> scores[i].second;
+
+    // sort by second asc, then first asc
     sort(scores, scores + K, [](const REST& lhs, const REST& rhs){
+        if(lhs.second != rhs.second) return lhs.second < rhs.second;
         return lhs.first < rhs.first;
     });
-    for(int i = 0; i < K; i++) cout << scores[i].first << " " << scores[i].second << endl;
-    long long max_y = scores[K - 1].second, ans = 0;
-    cout << max_y << "\n";
-    for(int i = K - 1; i > 0; i--){
-      if(scores[i].second > max_y){
-        max_y = scores[i].second;
-        ans++;
-      }
+
+    vector<int> BIT(K + 1);
+    long long ans = 0;
+
+    // now treat first[] as the permutation order, but since we just need
+    // relative inversions, we can traverse and count how many seen so far
+    // have first > current first
+    // we map positions 1..K in the current order to BIT indices
+    for(int i = K - 1; i >= 0; i--){
+        int pos = i + 1;
+        ans += RangeQuery(BIT, pos - 1);
+        PointUpdate(BIT, pos, 1);
     }
-    cout << ans;
+
+    cout << ans << "\n";
 }
--- Zerojudge/R628.cpp
+++ Zerojudge/R628.cpp
@@ -3,9 +3,8 @@
 #include <algorithm>
 using namespace std;
 
-const int MaxN = 2e5 + 1;
-
-int N, uN, num[MaxN * 2];
+const int MaxN = 1e5 + 1;
+int N, num[MaxN];
 
 int RangeQuery(vector<int>& BIT, int p){
     int ret = 0;
@@ -17,32 +16,25 @@ int RangeQuery(vector<int>& BIT, int p){
 }
 
 void PointUpdate(vector<int>& BIT, int p, int x){
-    while(p <= N){
-        BIT[p] += x;
-        p += p & -p;
+    int cur = p;
+    while(cur <= N){
+        if(cur == p) BIT[cur] += x;      // update position p normally
+        else BIT[cur] -= 1;      // decrement all other affected positions
+        cur += cur & -cur;
     }
 }
 
 int main(){
     cin >> N;
-    vector<pair<int, int>> idx(N + 1); 
-    vector<int> Inv_pair(2 * N + 1);
-    for(int n = 1; n <= 2 * N; n++){
-        cin >> num[n];
-        if(idx[num[n]].first != 0){
-            idx[num[n]].second = n;
-        } else {
-            idx[num[n]].first = n;
-        }
-    }
-    vector<int> BIT(N + 1);
+    for(int n = 1; n <= N; n++) cin >> num[n];
+    vector<int> BIT(N + 1, 0);
+    vector<int> Inv_pair(N + 1, 0);
+    // scan right to left: count how many already-seen values are < num[n]
     for(int n = N; n >= 1; n--){
         Inv_pair[n] = RangeQuery(BIT, num[n] - 1);
         PointUpdate(BIT, num[n], 1);
     }
     long long ans = 0;
-    for(int n = 1; n <= N; n++){
-        ans += Inv_pair[idx[n].first] - Inv_pair[idx[n].second]; 
-    }
+    for(int n = 1; n <= N; n++) ans += Inv_pair[n];
     cout << ans << "\n";
 }
